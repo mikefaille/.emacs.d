96d7fb1dc81785f23861240b3f59372d96e21bef
Michael Faille <michael@faille.pw>
2014-12-22 08:21:17 -0500

    add auto-complete as a complete working package and go mode as pkg too

---
+++ packages/pkg-ac-complete.el
@@ -0,0 +1,29 @@
+;;; package --- Suma auto-complete
+
+;;; Commentary:
+(require 'auto-complete)
+;;; Code:
+
+
+(defvar-local local-packages '(better-defaults smartparens idle-highlight-mode ido-ubiquitous find-file-in-project magit smex scpaste color-theme-solarized helm flycheck undo-tree dired-hacks-utils  yasnippet flycheck malabar-mode company company-go multiple-cursors go-mode go-autocomplete auto-complete emacs-eclim eshell-prompt-extras projectile fuzzy cl-lib deferred jedi ein ) "A list of packages to ensure are installed at launch.")
+
+
+
+(auto-complete-mode t)
+(ac-set-trigger-key "TAB")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
+(autoloadp 'auto-complete-config)
+(ac-config-default)
+(setq-default
+ ac-sources
+ '(
+   ac-source-yasnippet
+   ac-source-imenu
+   ac-source-abbrev
+   ac-source-words-in-same-mode-buffers
+   ac-source-files-in-current-dir
+   ac-source-filename
+   )
+)
+(provide 'pkg-ac-complete)
+;;; ac-complete ends here
---
+++ packages/pkg-go.el
@@ -0,0 +1,53 @@
+;;;; go config
+;; install godef 
+; go get code.google.com/p/rog-go/exp/cmd/godef
+; go get github.com/golang/lint/golint
+; autoloadp package : auto-complete
+
+;; go lint
+(require-package 'golint)
+
+
+(autoloadp 'go-mode)
+(add-hook 'before-save-hook 'gofmt-before-save)
+(autoloadp 'go-autocomplete)
+  (add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
+  (autoloadp 'golint)
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+
+
+(setenv "PATH"
+  ( concat 
+    "/usr/local/go/bin" ":"
+    (getenv "HOME") "/go/bin" ":"
+
+    (getenv "PATH")
+  )
+)
+
+(setenv "GOPATH"
+  ( concat 
+    (getenv "HOME") "/go" ":"
+
+    (getenv "GOPATH")
+  )
+)
+
+(add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/nsf/gocode"))
+
+;(executable-interpret "go get github.com/golang/lint/golint")
+
+;;(executable-interpret "go get github.com/kisielk/errcheck")
+
+
+(provide 'pkg-go)
