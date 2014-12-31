a2a38760467f07c9aca618682963798123602e43
Michael <michael@faille.io>
2014-12-31 03:16:58 -0500

    fix theme for mixed tty and X session

--- packages/pkg-theme.el
+++ packages/pkg-theme.el
@@ -1,11 +1,21 @@
-(require-package 'solarized-theme)
 (require-package 'color-theme-solarized)
+(require 'color-theme)
+(color-theme-initialize)
+
+;(require-package 'base16-theme)
+;;(solarized-dark)
+;;(require 'solarized-theme)
+;(load-theme 'solarized-dark t)
+
+
+;; deamon load without X
 (if (daemonp)
 (add-hook 'after-make-frame-functions
           '(lambda (f)
              (with-selected-frame f
-               (when (window-system f) (color-theme-solarized-dark)))))
-(color-theme-solarized-dark))
-
+               (when (window-system f) (load-theme 'solarized-dark t))))
+         (color-theme-tty-dark) )
+;; can add else...
+)
 
 (provide 'pkg-theme)
