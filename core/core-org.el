104a7c62c77fd8fa6e7c3512f076ffddb6ca56c7
Michael <michael@faille.io>
2015-01-03 03:39:55 -0500

    new modules and fix somes

---
+++ core/core-org.el
@@ -0,0 +1,10 @@
+
+;; active Babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((sh . t)
+   (python . t)
+   (plantuml . t)
+   ))
+
+(provide 'core-org)
---
+++ packages/pkg-latex.el
@@ -0,0 +1,60 @@
+(require-package 'auctex)
+(require-package 'cdlatex)
+(require-package 'auctex-latexmk)
+(require 'smartparens-latex)            ; smartparens suppose to be instaled automaticly
+(require 'tex-mik)
+
+;; sensible defaults for OS X, other OSes should be covered out-of-the-box
+(when (eq system-type 'darwin)
+  (setq TeX-view-program-selection
+      '((output-dvi "DVI Viewer")
+        (output-pdf "PDF Viewer")
+        (output-html "HTML Viewer")))
+(setq TeX-view-program-list
+      '(("DVI Viewer" "open %o")
+        ("PDF Viewer" "open %o")
+        ("HTML Viewer" "open %o"))))
+
+(defcustom latex-fast-math-entry 'LaTeX-math-mode
+  "Method used for fast math symbol entry in LaTeX."
+  :link '(function-link :tag "AUCTeX Math Mode" LaTeX-math-mode)
+  :link '(emacs-commentary-link :tag "CDLaTeX" "cdlatex.el")
+  :group 'pkg
+  :type '(choice (const :tag "None" nil)
+                 (const :tag "AUCTeX Math Mode" LaTeX-math-mode)
+                 (const :tag "CDLaTeX" cdlatex)))
+
+(defun prelude-latex-mode-defaults ()
+  "Default Prelude hook for `LaTeX-mode'."
+  (turn-on-auto-fill)
+  (abbrev-mode +1)
+  (smartparens-mode +1)
+  (case latex-fast-math-entry
+    (LaTeX-math-mode (LaTeX-math-mode 1))
+    (cdlatex (turn-on-cdlatex))))
+(setq prelude-latex-mode-hook 'latex-mode-defaults)
+
+
+(add-hook 'LaTeX-mode-hook (lambda ()
+  (push 
+    '("Latexmk" "latexmk -pdf %s" TeX-run-command nil t 
+      :help "Run Latexmk on file")
+    TeX-command-list)))
+
+
+;; AUCTeX configuration
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-engine 'xetex)
+(setq-default TeX-PDF-mode t)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+
+
+(provide 'pkg-latex)
