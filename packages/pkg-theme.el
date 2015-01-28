8580506073bf9bd0172f74a72c279d615bf6827f
Michael Faille <michael@faille.io>
2015-01-28 07:23:55 -0500

    themes..

--- packages/pkg-theme.el
+++ packages/pkg-theme.el
@@ -1,6 +1,5 @@
 ;(require-package 'solarized-theme)
 (require-package 'color-theme)
-(require 'color-theme)
 (require 'color-theme-solarized)
 ;(require 'color-theme-solarized)
 
@@ -9,7 +8,7 @@
 
 
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa")
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa")
 ;(set-background-color "black")
 
 ;; (lambda (frame) (set-frame-parameter frame 'background-mode dark) t)
@@ -51,19 +50,62 @@
 ;; ;; can add else...
 ;; ))
 
+;; (add-hook 'after-make-frame-functions
+;;           (lambda (frame)
+;;             (set-frame-parameter frame
+;;                                  'background-mode
+;;                                  'dark)
+;;             (enable-theme 'solarized))
+;;           )
+
+;; (add-hook 'after-make-frame-functions
+;;           (lambda (frame)
+;;             (set-frame-parameter frame
+;;                                  'background-mode
+;;                                  'dark))
+;;             (enable-theme 'solarized))
+
+;; (setq frame-background-mode "dark")
+;; ;(setq solarized-termcolors '256)
+;; ;(setq solarized-degrade nil)
+
+
+;; (load-theme 'solarized)
+;; (provide 'pkg-theme)
+
+
+
+
+;(set-background-color "black")
+
+(set-background-color "black")
 (add-hook 'after-make-frame-functions
+          (setq solarized-termcolors (if window-system '256 '16))
           (lambda (frame)
-            (if 'display-graphic-p      
             (set-frame-parameter frame
                                  'background-mode
-                                  'dark)
-
-            (enable-theme 'solarized)))
+                                 'dark)
+            (enable-theme 'solarized))
+            
           )
-(setq frame-background-mode 'dark)
-(setq solarized-termcolors '256)
-(setq solarized-degrade  nil)
-;;(set-background-color "black")
-
 (load-theme 'solarized)
+
+;; (unless window-system
+;;   (setq solarized-degrade (if window-system nil '16)
+;; )
+
+
+;;             (set-frame-parameter frame
+;;                                  'background-mode
+;;                                  'dark)
+
+;;             (enable-theme 'solarized))
+;;           )
+
+
+;(setq frame-background-mode "black")
+
+;(setq solarized-termcolors '256)
+;(setq solarized-degrade t)
+
 (provide 'pkg-theme)
