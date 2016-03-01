0a97138ea385f528b7f6ce6a5b5f46a140a9bb17
Michael Faille <michael@faille.io>
2016-03-01 14:19:57 -0500

    Refactory go pkg

--- packages/pkg-go.el
+++ packages/pkg-go.el
@@ -14,7 +14,6 @@
 (require-package 'golint)
 (require-package 'go-autocomplete)
 (require-package 'go-mode)
-(require-package 'exec-path-from-shell)
 
 (setenv "PATH"
   ( concat
@@ -30,72 +29,25 @@
     (getenv "HOME") "/go" ":"
 
     (getenv "GOPATH")
-  )
-)
+  ))
 
-
-
-
-
-;; helper function
-(defun go-run ()
-  "run current buffer"
-  (interactive)
-  (compile (concat "go run " (buffer-file-name))))
-(add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "C-c C-c") 'go-run)))
-
-
-(load   (concat (exec-path-from-shell-copy-env "GOPATH")  "/src/github.com/nsf/gocode/emacs/go-autocomplete.el"))
-
-
-;; wget https://github.com/golang/tools/blob/master/cmd/oracle/oracle.el
-;; using fedora ? install golang-googlecode-tools-oracle package
-;; tutorial http://tleyden.github.io/blog/2014/05/27/configure-emacs-as-a-go-editor-from-scratch-part-2/
-
-
-(load (concat
-       (exec-path-from-shell-copy-env "GOPATH") "/src/golang.org/x/tools/cmd/oracle/oracle.el"))
-;; (require 'oracle)
-
-
-
-
-;;(exe)
-;;(exec-path-from-shell-initialize)
-(add-to-list 'exec-path (concat
-                         (exec-path-from-shell-copy-env "GOPATH") "/bin"))
-;;(exec-path-from-shell-copy-env "GOPAT")
-;(executable-interpret "go get github.com/golang/lint/golint")
-
-
-(add-hook 'before-save-hook 'gofmt-before-save)
 
 
 ;; (require-package go-errcheck)
 ;;(executable-interpret "go get github.com/kisielk/errcheck")
 
 
-
-;go-remove-unused-imports
-;; Instead of offering a function for removing a single import, go-mode will detect all unused imports and delete them (or comment them) once you run go-remove-unused-imports. It is not bound to a key by default, but you can bind it yourself if you want to. Personally I have bound it to C-c C-r:
-(add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-
 ; go-goto-imports
 ;; If you decide you want to look at your imports or edit them manually, go-goto-imports will take you to them automatically, placing your cursor after the last import. It isn’t bound to a key, either, mainly because I couldn’t come up with a good default that didn’t violate Emacs guidelines. But you can bind it manually, just like before:
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c i") 'go-goto-imports)))
 
-;; eldoc for go
-(require-package 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
 
 
+(with-eval-after-load 'go-mode
 
-(eval-after-load 'go-mode
-  '(progn
-
+     (require-package 'go-eldoc)
+     (go-eldoc-setup)
      ;; Add to default go-mode key bindings
      (let ((map go-mode-map))
        (define-key map (kbd "C-c a") 'go-test-current-project) ;; current package, really
@@ -113,29 +65,53 @@
      (whitespace-toggle-options '(tabs))
 
 
+     ;; TODO Fix this path
+     (add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
+     ;; Customize compile command to run go build
 
-
-       (add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
-       ;; Customize compile command to run go build
-       (defun my-go-mode-hook ()
-
-                                        ; Use goimports instead of go-fmt
-         (setq gofmt-command "goimports")                                      ; Call Gofmt before saving
-         (add-hook 'before-save-hook 'gofmt-before-save)
+     (add-hook 'before-save-hook 'gofmt-before-save)
                                         ; Customize compile command to run go build
-         (if (not (string-match "go" compile-command))
-             (set (make-local-variable 'compile-command)
-                  "go build -v && go test -v && go vet"))
+     (if (not (string-match "go" compile-command))
+         (set (make-local-variable 'compile-command)
+              "go build -v && go test -v && go vet"))
                                         ; Godef jump key binding
-         (local-set-key (kbd "M-.") 'godef-jump))
-       (add-hook 'go-mode-hook 'my-go-mode-hook)
+     (local-set-key (kbd "M-.") 'godef-jump)
 
-       ;; Enable go-oracle-mode if available
-       (let ((oracle (executable-find "oracle")))
-         (when oracle
-         (setq go-oracle-command oracle)
-         )))
+
+  (let ((file-oracle "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el"))
+    (when (file-exists-p file-oracle)
+      (load-file file-oracle)
+
+      )
+    )
+
+  (let ((oracle (executable-find "oracle")))
+    (when oracle
+      (setq go-oracle-command oracle)
+      ))
+
+
+  (let ((file-go-autocomplete "$GOPATH/src/github.com/nsf/gocode/emacs/go-autocomplete.el"))
+    (when (file-exists-p file-go-autocomplete)
+      (load-file file-go-autocomplete)
+      )
+    )
+
+
+  ;; helper function
+  (defun go-run ()
+    "run current buffer"
+    (interactive)
+    (compile (concat "go run " (buffer-file-name))))
+
+  (local-set-key (kbd "C-c C-c") 'go-run)
+
+  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+  ;; Enable go-oracle-mode if available
+
+
   )
+
 
 
 (provide 'pkg-go)
