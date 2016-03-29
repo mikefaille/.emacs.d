21066f6b5b4aa7a2a740f950ec638c70a1b343ef
Michael Faille <michael@faille.io>
2016-03-29 07:34:56 -0400

    org : Complete refactor

--- packages/pkg-org.el
+++ packages/pkg-org.el
@@ -3,41 +3,109 @@
 
 
 ;; dont use org from melpa : https://github.com/purcell/emacs.d/issues/297
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((R . t)
-   (emacs-lisp . t)
-   (ruby . t)
-   (sh . t)
-   ))
+
+
 
 (require 'org-protocol)
 (require 'org-capture)
-(setq org-capture-templates
-      (quote
-       (("w"
-         "Default template"
-         entry
-         (file+headline "~/org/capture.org" "Notes")
-         "* %^{Title}\n\n  Source: %u, %c\n\n  %i"
-         :empty-lines 1)
-        ;; ... more templates here ...
-        )))
+(require-package 'org-bullets)
+
+
+(setq org-directory "~/owncloud/org")
+
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(set-register ?o (cons 'file (concat org-directory "/notes.org")))
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
+(ido-mode)
+
+
+(setq org-agenda-files (concat org-directory "/task.org"))
+
+
+(with-eval-after-load 'org-capture-mode
+
+  (setq org-capture-templates
+        (quote
+         (("w"
+           "Default template"
+           entry
+           (file+headline org-capture-default-file "Notes")
+           "* %^{Title}\n\n  Source: %u, %c\n\n  %i"
+           :empty-lines 1)
+          ;; ... more templates here ...
+          )))
+
+  )
+
+
+
+(with-eval-after-load 'deft-mode
+  ;; Deft config
+  (require-package 'deft)
+  (setq deft-directory org-directory)
+  (setq deft-default-extension "org")
+  (setq deft-extensions (quote ("org" "txt" "text" "md" "markdown")))
+
+  )
+
+(defvar org-capture-default-file (concat org-directory "/capture.org"))
+
 
 ;; (global-set-key (kbd "C-c o")
 ;;                 (lambda () (interactive) (find-file "~/organizer.org")))
 
-(set-register ?o (cons 'file "~/organizer.org"))
-(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
-(ido-mode)
-(setq org-completion-use-ido t)
-
 
 (global-set-key (kbd "C-c c") 'org-capture)
 
-(setq org-default-notes-file "~/organizer.org")
 
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+
+(with-eval-after-load 'org
+
+
+
+  ;; Org-Babel
+
+  ;; Fontifying source blocks
+
+  ;; Enable syntax highlighting in src blocks.
+
+  (setq-default org-src-fontify-natively t)
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((R . t)
+     (emacs-lisp . t)
+     (ruby . t)
+     (sh . t)
+     ))
+
+
+  ;; Notes / Tasks / TODOs
+
+  ;; Make custom markers for todo items:
+
+  ;; TODO
+  ;; something that needs to be done at some point. If it has a date, it should be done on that day but it may be moved.
+  ;; PENDING
+  ;; something that’s awaiting feedback from someone else. If it has a date, it needs followup if there hasn’t been any feedback at that time.
+  ;; MEETING
+  ;; a scheduled meeting and cannot easily be rescheduled.
+  ;; DOING
+  ;; In progress, needs to be finished
+  ;; DONE
+  ;; done.
+  ;; CANCELED
+  ;; can be ignored. May include a note on why it’s been cancelled.
+
+
+
+
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "DOING(i)" "PENDING(p)" "MEETING(m)" "|" "DONE(d)" "CANCELED(c)")))
+  )
 
 (provide 'pkg-org)
 ;;
