b6c6504cfbb98a353be251577d3acf5a35eee8f2
Michael Faille <michael@faille.io>
2016-03-31 07:10:11 -0400

    Add all latex engine in queue to export org file

--- packages/pkg-org.el
+++ packages/pkg-org.el
@@ -20,6 +20,16 @@
 (set-register ?o (cons 'file (concat org-directory "/notes.org")))
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
 
+(setq org-latex-pdf-process '(
+                              "latexmk -pdflatex='lualatex -shell-escape -interaction nonstopmode' -pdf -f  %f"
+                              "LATEX=lualatex texi2dvi -p -b -V %f"
+                              "latexmk -pdflatex='xetex -shell-escape -interaction nonstopmode' -pdf -f  %f"
+                              "LATEX=xelatex texi2dvi -p -b -V %f"
+                              "latexmk -pdf -f  %f"
+                              "texi2dvi -p -b -V %f"
+                               )
+      )
+
 
 
 (setq org-agenda-files (concat org-directory "/task.org"))
@@ -64,44 +74,67 @@
 
 (with-eval-after-load 'org
 
-
-  (org-bullets-mode)
-  ;; Org-Babel
-
-  ;; Fontifying source blocks
-
-  ;; Enable syntax highlighting in src blocks.
-
-  (setq-default org-src-fontify-natively t)
-
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((R . t)
-     (emacs-lisp . t)
-     (ruby . t)
-     (sh . t)
-     ))
-
-
-  ;; Notes / Tasks / TODOs
-
-  ;; Make custom markers for todo items:
-
-  ;; TODO
-  ;; something that needs to be done at some point. If it has a date, it should be done on that day but it may be moved.
-  ;; PENDING
-  ;; something that’s awaiting feedback from someone else. If it has a date, it needs followup if there hasn’t been any feedback at that time.
-  ;; MEETING
-  ;; a scheduled meeting and cannot easily be rescheduled.
-  ;; DOING
-  ;; In progress, needs to be finished
-  ;; DONE
-  ;; done.
-  ;; CANCELED
-  ;; can be ignored. May include a note on why it’s been cancelled.
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "DOING(i)" "PENDING(p)" "MEETING(m)" "|" "DONE(d)" "CANCELED(c)")))
   )
+
+
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-bullets-mode 1)
+
+            (visual-line-mode t)
+            ;; Org-Babel
+
+            ;; Fontifying source blocks
+
+            ;; Enable syntax highlighting in src blocks.
+
+            (setq-default org-src-fontify-natively t)
+
+            (org-babel-do-load-languages
+             'org-babel-load-languages
+             '((R . t)
+               (emacs-lisp . t)
+               (ruby . t)
+               (sh . t)
+               ))
+
+
+            ;; Notes / Tasks / TODOs
+
+            ;; Make custom markers for todo items:
+
+            ;; TODO
+            ;; something that needs to be done at some point. If it has a date, it should be done on that day but it may be moved.
+            ;; PENDING
+            ;; something that’s awaiting feedback from someone else. If it has a date, it needs followup if there hasn’t been any feedback at that time.
+            ;; MEETING
+            ;; a scheduled meeting and cannot easily be rescheduled.
+            ;; DOING
+            ;; In progress, needs to be finished
+            ;; DONE
+            ;; done.
+            ;; CANCELED
+            ;; can be ignored. May include a note on why it’s been cancelled.
+
+            (setq org-todo-keywords '((sequence "☛ TODO(t)" "|" "✔ DONE(d)")
+                                      (sequence "⚑ WAITING(w)" "|")
+                                      (sequence "|" "✘ CANCELED(c)")))
+
+
+            ))
+
+;; (defun set-org-agenda-files ()
+;;   "Set agenda files using org folder."
+;;   (setq org-agenda-files  (list (directory-files org-directory t ".org$")
+;; )
+;;         ))
+
+
+;; (add-hook 'org-agenda-mode-hook (set-org-agenda-files))
+
+;; (remove-hook 'org-agenda-mode-hook set-org-agenda-files))
 
 (provide 'pkg-org)
 ;;
