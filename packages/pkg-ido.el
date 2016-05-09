6252adffd9fa5cd3bd0dc4fceaf4e72157d2b9df
Michaël Faille <michael@faille.io>
2016-05-08 23:55:48 -0400

    Add pkg-ido

---
+++ packages/pkg-ido.el
@@ -0,0 +1,22 @@
+(require-package 'smex)
+(require-package 'browse-kill-ring+)
+(require-package 'flx-ido)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(ido-mode 1)
+(ido-everywhere 1)
+
+
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces t)
+
+
+(provide 'pkg-ido)
