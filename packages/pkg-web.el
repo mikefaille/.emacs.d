a549d3a5e4494f2953aff893ce425e6300b1076e
Michaël Faille <michael@faille.io>
2016-05-08 23:50:17 -0400

    Improvement -  web : Use hook instead of static load from init

--- packages/pkg-web.el
+++ packages/pkg-web.el
@@ -1,42 +1,47 @@
 (require-package 'web-mode)
-(require-package 'css-eldoc)
-(require-package 'emmet-mode)
+(add-hook 'web-mode-hook (lambda ()
+                           (whitespace-mode -1)
+                           (require-package 'css-eldoc)
+                           (require-package 'emmet-mode)
 
-;;(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
+                           ;;(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
 
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 
-(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
-
-
-
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(setq web-mode-engines-alist
-      '(("php"    . "\\.phtml\\'")
-        ("blade"  . "\\.blade\\."))
-      )
+                           (add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
 
 
 
-(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+                           (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+                           (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+                           (setq web-mode-engines-alist
+                                 '(("php"    . "\\.phtml\\'")
+                                   ("blade"  . "\\.blade\\."))
+                                 )
 
 
-(require 'smart-tab)
-(defun add-emmet-expand-to-smart-tab-completions ()
-  ;; Add an entry for current major mode in
-  ;; `smart-tab-completion-functions-alist' to use
-  ;; `emmet-expand-line'.
-  (add-to-list 'smart-tab-completion-functions-alist
-               (cons major-mode #'emmet-expand-line)))
-(add-hook 'sgml-mode-hook 'add-emmet-expand-to-smart-tab-completions)
 
-(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+                           (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+
+
+                           (require 'smart-tab)
+                           (defun add-emmet-expand-to-smart-tab-completions ()
+                             ;; Add an entry for current major mode in
+                             ;; `smart-tab-completion-functions-alist' to use
+                             ;; `emmet-expand-line'.
+                             (add-to-list 'smart-tab-completion-functions-alist
+                                          (cons major-mode #'emmet-expand-line)))
+                           (add-hook 'sgml-mode-hook 'add-emmet-expand-to-smart-tab-completions)
+
+                           (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+
+
+                           ))
 
 (provide 'pkg-web)
