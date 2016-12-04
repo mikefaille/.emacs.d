7b6774f8f7ed83a2eadee73a32698cf4a2c824d4
Michaël Faille <michael@faille.io>
2016-12-04 01:02:27 -0500

    Yaml lint : fixed. Should be ok with yamlint default config

--- packages/pkg-yaml.el
+++ packages/pkg-yaml.el
@@ -1,4 +1,22 @@
 (require-package 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+;; (flycheck-define-checker yaml-mode
+;;   "Erlang syntax checker based on dialyzer."
+;;   :command ("yamllint" "-f" "parsable"
+;;             "-d" "{extends: relaxed, rules: {line-length: {max: 120}}}" source-original)
+;;   :error-patterns
+;;   ((error line-start
+;;           (file-name)
+;;           ":"
+;;           line
+;;           ":"
+;;           column
+;;           ":"
+;;           (message)
+
+;;           line-end))
+;;   :modes yaml-mode)
+;; (add-to-list 'flycheck-checkers 'erlang-dialyzer t)
+
 (provide 'pkg-yaml)
