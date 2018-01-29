039b9c2db4a2f3a4341b3928afb1387519d7a75e
Michael Faille <michael@faille.io>
2018-01-29 12:06:14 -0500

    yamlint : adding hook to load only when needed

--- packages/pkg-yaml.el
+++ packages/pkg-yaml.el
@@ -1,5 +1,19 @@
 (require-package 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+
+(add-hook 'yaml-mode-hook
+	  (lambda ()
+	    (require-package 'indent-tools)
+	    (local-set-key (kbd "C-c >") 'indent-tools-hydra/body)
+
+
+	    (require-package 'flycheck-yamllint)
+	    (eval-after-load 'flycheck
+	      '(add-hook 'flycheck-mode-hook 'flycheck-yamllint-setup))
+
+	    ))
 
 ;; (flycheck-define-checker yaml-mode
 ;;   "Erlang syntax checker based on dialyzer."
