3bf1f29fd7e0a6d397010189fdb223cf6e9c1a21
Michaël Faille <michael@faille.io>
2018-06-24 23:08:12 -0400

    python-mode: use elpa over jedi as main mode

--- packages/pkg-python.el
+++ packages/pkg-python.el
@@ -1,17 +1,22 @@
-(require-package 'jedi)
+;; (require-package 'jedi)
+(require-package 'elpy)
+(require-package 'py-autopep8)
+
+;; Load python-mode
+(add-hook 'python-mode-hook 'elpy)
+(when (require 'elpy nil t)
+  (elpy-enable))
+
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; For elpy
+(setq elpy-rpc-python-command "python3")
+;; For interactive shell
+(setq python-shell-interpreter "python3")
+(setq elpy-rpc-backend "jedi")
+(setq python-shell-interpreter "ipython3"
+      python-shell-interpreter-args "-i --simple-prompt")
 
 
-
-;; (require-package 'elpy)
-;; (package-initialize)
-;; (elpy-enable)
-
-;;python jedi for completion
-(autoload 'jedi:setup "jedi" nil t)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
-(autoloadp 'ein)
 (setq ein:use-auto-complete-superpack t)
 (setq ein:use-smartrep t)
 (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
@@ -20,16 +25,13 @@
 ;; (setq-default indent-tabs-mode nil)
 ;; (setq-default tab-width 4)
 
+(setq py-autopep8-options '("--max-line-length=120"))
 
 ;; pip install jedi
-;; # flake8 for code checks
-;; pip install flake8
+;; # autoflake8 for code checks
+;; pip install autoflake8
 ;; # and importmagic for automatic imports
 ;; pip install importmagic
 ;; https://github.com/jorgenschaefer/elpy
-
-(require-package 'py-autopep8)
-(setq py-autopep8-options '("--max-line-length=120"))
-
 
 (provide 'pkg-python)
