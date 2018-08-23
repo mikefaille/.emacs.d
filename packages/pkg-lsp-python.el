9b0f0740dd49087b0e42705176791d6f6d24c3ab
Michaël Faille <michael@faille.io>
2018-08-22 23:01:05 -0400

    lsp-python fix : lsp-python dont load automatically.

--- packages/pkg-lsp-python.el
+++ packages/pkg-lsp-python.el
@@ -1,6 +1,7 @@
 (require 'lsp-mode)
 (require 'lsp-ui)
 (require-package 'lsp-python)
+(require 'lsp-python)
 (add-hook 'python-mode-hook #'lsp-python-enable)
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
