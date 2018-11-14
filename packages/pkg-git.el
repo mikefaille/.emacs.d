a163f8e5f59c20be75c749317841f7b4401605a1
Michaël Faille <michael@faille.io>
2018-11-14 05:50:34 -0500

    git-gutter: add support for prog-mode
    not only for ruby-mode

--- packages/pkg-git.el
+++ packages/pkg-git.el
@@ -10,7 +10,7 @@
 ;; (git-gutter:linum-setup)
 
 ;; If you enable git-gutter-mode for some modes
-(add-hook 'ruby-mode-hook 'git-gutter-mode)
+(add-hook 'prog-mode-hook 'git-gutter-mode)
 
 (global-set-key (kbd "C-x C-g") 'git-gutter-mode)
 (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
