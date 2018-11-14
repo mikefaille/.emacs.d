c791b74ac0b832df6f20b783ec300377da7dc77a
Michaël Faille <michael@faille.io>
2018-11-14 06:05:39 -0500

    eply : fix unavalable repo bloc emacs load.
    It bloc especially remacs.
    
    It's and old repo elpy repo. Now, we can retreive elpy on melpa

--- core/core-packages.el
+++ core/core-packages.el
@@ -51,8 +51,6 @@
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 
 
 ; activate all the packages (in particular autoloads)
