f833743f8158598d98c518135119d1c904f71a66
Michael Faille <michael@faille.io>
2022-06-14 23:29:26 -0700

    fix indent via tab for company-mode

--- packages/pkg-company.el
+++ packages/pkg-company.el
@@ -1,4 +1,7 @@
 (require-package 'company)
+(require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
+(define-key company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common)
+
 (setq company-idle-delay 0.3)
 (provide 'pkg-company)
