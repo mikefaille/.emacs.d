cbcee419681d84a5be9278a91ace929a98720589
Michael Faille <michael@faille.io>
2022-06-14 19:55:16 -0700

    maintain the projectile mode
    - fix "persp" is not supported anymore. Just delete it.
    - load faster nixos configs

--- packages/pkg-projectile.el
+++ packages/pkg-projectile.el
@@ -2,15 +2,14 @@
 
 (require-package 'projectile)
 (require-package 'go-projectile)
-(projectile-global-mode t)
-;; (require 'go-projectile)
+(projectile-mode t)
+
 (require-package 'exec-path-from-shell)
 
 (setq go-projectile-tools-path (concat (exec-path-from-shell-copy-env "HOME") "/go"))
 ;(require 'go-projectile-autoloads)
 
 
-(projectile-global-mode)
 (setq projectile-completion-system 'ido)
 
 ;; (setq projectile-keymap-prefix (kbd "C-c C-p"))
@@ -23,12 +22,15 @@
 
 ;; (define-key projectile-mode-map (kbd "s-x") 'projectile-persp-switch-project)
 
-(define-key projectile-mode-map (kbd "C-<tab>") 'persp-next)
-(define-key projectile-mode-map (kbd "<C-iso-lefttab>") 'persp-prev)
+;; (define-key projectile-mode-map (kbd "C-<tab>") 'persp-next)
+;; (define-key projectile-mode-map (kbd "<C-iso-lefttab>") 'persp-prev)
 
 
 
 (setq projectile-cache-file (expand-file-name "projectile.cache" main-savefile-dir))
 
+
+(setq projectile-project-search-path '("~/src" "/etc/nixos"))
+(define-key projectile-mode-map (kbd "C-<tab>") 'projectile-persp-switch-project)
 
 (provide 'pkg-projectile)
