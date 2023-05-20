4050e932be167c4238be0cf43e921946d199ad10
Michael Faille <michael@faille.io>
2023-05-20 12:37:26 -0700

    get the dark frame again

--- packages/pkg-theme.el
+++ packages/pkg-theme.el
@@ -2,4 +2,16 @@
 
 (load-theme 'solarized-dark t)
 
+
+(defun set-emacs-frames (variant)
+  (dolist (frame (frame-list))
+    (let* ((window-id (frame-parameter frame 'outer-window-id))
+	   (id (string-to-number window-id))
+	   (cmd (format "xprop -id 0x%x -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"%s\""
+			id variant)))
+      (call-process-shell-command cmd))))
+
+(set-emacs-frames "dark")
+
+
 (provide 'pkg-theme)
