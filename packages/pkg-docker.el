b801949094066e5a1891c69fada87c0b4ba1eac9
Michael Faille <michael@faille.io>
2023-05-20 12:36:43 -0700

    fix the indent in docker

--- packages/pkg-docker.el
+++ packages/pkg-docker.el
@@ -40,9 +40,11 @@
 (require-package 'docker)
 
 
-
-
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
+(add-hook 'dockerfile-mode-hook '(lambda()
+																	 (setq-local sh-basic-offset 2)
+																	 (setq-local indent-line-function #'sh-indent-line)
+																	 ))
 
 (provide 'pkg-docker)
